b88a01767a0410c18c662db675d3ad622796b896
charliesands <cwsands3@gmail.com>
2018-10-11 19:06:11 -0600

    added modal to vaultkeeps

--- wwwsrc/src/views/Dashboard.vue
+++ wwwsrc/src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="dashboard">
+  <div class="dashboard container-fluid">
     <div>
-      <h1>Hello?</h1>
+      <h1>Dashboard</h1>
     </div>
     <div>
       <form @submit.prevent>
@@ -12,12 +12,47 @@
         <input type="submit" @click="getVaultKeeps(chosen)">
       </form>
     </div>
-    <div>show please</div>
+    <div class="row">
+      <div v-for="vk in vaultKeeps" :key="vk._id" class="col-3">
+        <div @click="viewKeep(vk)" data-toggle="modal" :data-target="'#vk'+vk.id">
+          <img :src="vk.img">
+        </div>
+        <div>
+          <h3> {{vk.name}}</h3>
+        </div>
+        <div>
+          <p> shares:{{vk.shares}} views:{{vk.views}} keeps:{{vk.keeps}} </p>
+        </div>
+        <button type="button" class="btn btn-primary" @click="addShare(vk)">Share</button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" :data-target="'#vk'+vk.id" @click="viewKeep(vk)">View</button>
+        <!-- <button type="button" class="btn btn-primary">Keeps</button> -->
 
-    <div v-for="vk in vaultKeeps" :key="vk._id" class="col-3">
-      hello ?? {{vk.name}} {{vk.description}}
+        <div class="modal fade" :id="'vk'+vk.id" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+          aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{vk.name}}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div>
+                  <img :src="vk.img">
+                </div>
+                <div>
+                  {{vk.description}}
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
   </div>
 </template>
 
@@ -46,6 +81,14 @@
     methods: {
       getVaultKeeps(vault) {
         this.$store.dispatch('getVaultKeeps', vault)
+      },
+      addShare(vk) {
+        keep.shares++
+        this.$store.dispatch("updateKeep", vk)
+      },
+      viewKeep(vk) {
+        keep.views++
+        this.$store.dispatch("updateKeep", vk)
       }
     }
   }
